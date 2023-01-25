ffc848287bfe416c820b20767a72d34ce37403e2
Gleb Ivashyn <gleb.ivashyn@stud.vgtu.lt>
2023-01-25 11:55:54 +0100

    fetch city coordinates initial

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 let tableBody = document.getElementById("tbody");
 let rowIndex = 0;
 
+// fetch('../resources/initialProjects.json')
+//     .then(response => response.json())
+//     .then(json => {
+//         console.log(json)
+//     })
+
+fetchCityInfo()
 self.addEventListener("message", (ev)=>{
     //retrieving submitted data
     const fieldsJson = ev.data;
@@ -8,9 +15,12 @@ self.addEventListener("message", (ev)=>{
     const projectName = formFieldsObject.projectName;
     const city = formFieldsObject.city;
     const description = formFieldsObject.projectDescription;
+
     console.log(projectName);
     console.log(city)
     console.log(description)
+
+    fetchCityInfo()
 
     //storing data to local storage
     localStorage.setItem(projectName, fieldsJson);
@@ -19,3 +29,15 @@ self.addEventListener("message", (ev)=>{
     // setTimeout(() => updateTable(), 500);
 
 })
+
+function fetchCityInfo(){
+    fetch(`http://nominatim.openstreetmap.org/search?q=paris&limit=1&format=json`)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json[0])
+
+            console.log(json[0].lat)
+            console.log(json[0].lon)
+        })
+
+}
